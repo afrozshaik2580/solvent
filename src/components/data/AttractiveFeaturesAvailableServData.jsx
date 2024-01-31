a8b4fd3e1f65dcafdbc69cf5869e339d3c55d86c
Afroz Shaik <102058216+afrozshaik2580@users.noreply.github.com>
2024-01-31 22:17:31 +0530

    Add files via upload

--- src/components/data/AttractiveFeaturesAvailableServData.jsx
+++ src/components/data/AttractiveFeaturesAvailableServData.jsx
@@ -1,52 +1,62 @@
+import service1 from '../../assets/service1.png';
+import service2 from '../../assets/service2.png';
+import service3 from '../../assets/service3.png';
+import Vector1 from '../../assets/Vector1.png';
+import Vector from '../../assets/Vector.png';
+import Vector2 from '../../assets/Vector2.png';
+import Vector3 from '../../assets/Vector3.png';
+import Vector4 from '../../assets/Vector4.png';
+import Vector5 from '../../assets/Vector5.png';
+
 export const availableServices_cardData = [
     {
       id: 1,
       title: 'UI UX',
       price: '$59.99',
       description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam porro est quasi saepe maiores doloremque earum nostrum ',
-      linkImg:'src/assets/service1.png'
+      linkImg: service1
     },
     {
       id: 2,
       title: 'Sales',
       price: '$59.99',
       description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam porro est quasi saepe maiores doloremque earum nostrum ',
-      linkImg:'src/assets/service2.png'
+      linkImg: service2
      },
     {
       id: 3,
       title: 'Digital Marketing',
       price: '$59.99',
       description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam porro est quasi saepe maiores doloremque earum nostrum ',
-      linkImg:'src/assets/service3.png'
+      linkImg:service3
     },
     {
         id: 4,
         title: 'Sales',
         price: '$59.99',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam porro est quasi saepe maiores doloremque earum nostrum ',
-        linkImg:'src/assets/service2.png'
+        linkImg:service2
     },
     {
         id: 5,
         title: 'Digital Marketing',
         price: '$59.99',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam porro est quasi saepe maiores doloremque earum nostrum ',
-        linkImg:'src/assets/service3.png'
+        linkImg:service3
     },
     {
         id: 6,
         title: 'UI UX',
         price: '$59.99',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam porro est quasi saepe maiores doloremque earum nostrum ',
-        linkImg:'src/assets/service1.png'
+        linkImg: service1
       },
     {
       id: 7,
         title: 'Sales',
         price: '$59.99',
         description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam porro est quasi saepe maiores doloremque earum nostrum ',
-        linkImg:'src/assets/service2.png'
+        linkImg: service2
     },
   ];
 
@@ -55,37 +65,37 @@ export const availableServices_cardData = [
 
   export const attractiveFeatures_cardData =[
     {
-        imageUrl:'src/assets/Vector1.png',
+        imageUrl:Vector1,
         cardtitle: 'Interactive',
         cardSubtitle:' Elements',
         cardBody:'Eu tellus quam id sed ultrices. Integer nunc lectus nisi, erat et ornare risus.',
       },
       {
-        imageUrl:'src/assets/Vector.png',
+        imageUrl: Vector,
        cardtitle: 'Interactive',
         cardSubtitle:' Elements',
         cardBody:'Eu tellus quam id sed ultrices. Integer nunc lectus nisi, erat et ornare risus.',
       },
       {
-        imageUrl:'src/assets/Vector2.png',
+        imageUrl: Vector2,
        cardtitle: 'Interactive',
         cardSubtitle:' Elements',
         cardBody:'Eu tellus quam id sed ultrices. Integer nunc lectus nisi, erat et ornare risus.',
       },
       {
-        imageUrl:'src/assets/Vector3.png',
+        imageUrl: Vector3,
        cardtitle: 'Interactive',
         cardSubtitle:' Elements',
         cardBody:'Eu tellus quam id sed ultrices. Integer nunc lectus nisi, erat et ornare risus.',
       },
       {
-        imageUrl:'src/assets/Vector4.png',
+        imageUrl: Vector4,
        cardtitle: 'Interactive',
         cardSubtitle:' Elements',
         cardBody:'Eu tellus quam id sed ultrices. Integer nunc lectus nisi, erat et ornare risus.',
       },
       {
-        imageUrl:'src/assets/Vector5.png',
+        imageUrl: Vector5,
        cardtitle: 'Interactive',
         cardSubtitle:' Elements',
         cardBody:'Eu tellus quam id sed ultrices. Integer nunc lectus nisi, erat et ornare risus.',
--- src/components/data/TestimonialWhatMakesDifference.jsx
+++ src/components/data/TestimonialWhatMakesDifference.jsx
@@ -1,48 +1,51 @@
-import { WhatMakesUsDifference } from "../Testimonial";
+// import { WhatMakesUsDifference } from "../Testimonial";
+import img1 from '../../assets/profileImage1.jpg';
+import img2 from '../../assets/profileImage2.jpg';
+import img3 from '../../assets/profileImage3.jpeg';
+
 
 export const TestimonialCardData = [
     {
       id: 1,
       title: 'Courtney Henry',
       description: 'CEO, Company',
-      linkImg: 'src/assets/pexels-pixabay-220453.jpg'
+      linkImg: img1
     },
     {
       id: 2,
       title: 'Courtney Henry',
       description: 'CEO, Company',
-      linkImg:'src/assets/profile_user.jpg'
+      linkImg:img2
      },
     {
       id: 3,
       title: 'Digital Marketing',
       description: 'CEO, Company',
-      linkImg:'src/assets/th.jpeg'
+      linkImg: img3
     },
     {
         id: 4,
         title: 'Courtney Henry',
         description: 'CEO, Company',
-        linkImg:'src/assets/service2.png'
+        linkImg: img2
     },
     {
         id: 5,
         title: 'Digital Marketing',
-
         description: 'CEO, Company',
-        linkImg:'src/assets/service3.png'
+        linkImg:img1
     },
     {
         id: 6,
         title: 'UI UX',
         description: 'CEO, Company',
-        linkImg:'src/assets/service1.png'
+        linkImg:img3
       },
     {
       id: 7,
         title: 'Courtney Henry',
         description: 'CEO, Company',
-        linkImg:'src/assets/service2.png'
+        linkImg:img1
     },
   ];
 
