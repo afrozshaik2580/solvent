d92100fc88f25a435c5ef969a8091eeca4a5165f
Afroz Shaik <102058216+afrozshaik2580@users.noreply.github.com>
2024-02-13 20:26:30 +0530

    Update BrandingSection.jsx

--- src/components/BrandingSection.jsx
+++ src/components/BrandingSection.jsx
@@ -1,12 +1,12 @@
 export default function BrandingSection() {
   return (
-    <div className="sm:h-[650px] md:h-[550px] sm:py-20 px-20 lg:px-28 py-28 relative">
-      <div className="h-1/3">
-        <h1 className="sm:text-5xl md:text-6xl lg:text-7xl m-0">
+    <div className="sm:py-20 sm:px-4 md:px-14 lg:px-20 py-4 mb-5 relative">
+      <div>
+        <h1 className="sm:text-5xl md:text-6xl lg:text-7xl my-4">
           The Art of Branding
         </h1>
       </div>
-      <div className="h-2/3 md:pr-[30%]">
+      <div className="md:pr-[30%]">
         <p className="sm:text-2xl lg:text-3xl font-light">
           At Solvent, our mission is to Digital Transmission. We believe in
           harnessing the power of the essence of successful online presence and
@@ -23,7 +23,7 @@ export default function BrandingSection() {
 function AnimatedCircles() {
   return (
     <>
-      <div className="absolute bottom-[85px] right-20 w-[300px] h-[300px] sm:opacity-15 md:opacity-100">
+      <div className="absolute bottom-[45px] right-20 w-[300px] h-[300px] sm:opacity-15 md:opacity-100">
         <div className="relative w-full h-full ">
           <div className="absolute top-0 left-0 w-[300px] h-[300px] border-[1px] border-white rounded-full"></div>
           <div className="absolute top-[50px] left-[50px] w-[200px] h-[200px] border-[1px] border-white rounded-full"></div>
@@ -31,7 +31,7 @@ function AnimatedCircles() {
         </div>
       </div>
       {/* [animation:spin_0.6s_linear_infinite] [transform:rotate(180deg)_rotateY(180deg)] */}
-      <div className="absolute bottom-[60px] right-14 w-[350px] h-[350px] sm:opacity-15 md:opacity-100 md:[transform:rotate(180deg)_rotateY(180deg)] md:animate-spin">
+      <div className="absolute bottom-[20px] right-14 w-[350px] h-[350px] sm:opacity-15 md:opacity-100 md:[transform:rotate(180deg)_rotateY(180deg)] md:animate-spin">
         <div className="relative w-full h-full">
           <div className="absolute top-0 left-0 w-[350px] h-[350px] border-[1px] border-white rounded-full"></div>
           <div className="absolute top-[50px] left-[50px] w-[250px] h-[250px] border-[1px] border-white rounded-full"></div>
